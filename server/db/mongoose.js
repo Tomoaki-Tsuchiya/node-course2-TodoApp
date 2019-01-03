705fd3edfdd8c42394ab3b4c707e524c22877e11
Tomoaki-Tsuchiya <tomt0m.25.g00cl@gmail.com>
2019-01-03 18:41:51 +0900

    Setup for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://tomotaro:tom0test@ds041140.mlab.com:41140/todoapp');
 
 module.exports = {mongoose};
